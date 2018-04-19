35ef7ddae04d4152fb28aa55c0962543c5e7fd74
Your Name <you@example.com>
2018-04-19 11:38:45 -0300

    Pratica estelar

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,17 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+let botoes = document.querySelectorAll('.botao-expandir-retrair');
+let botaoClicado = (e) => {
+  if (e.currentTarget.innerText === '+') {
+    e.currentTarget.innerText = '-';
+    e.currentTarget.parentElement.classList.add('expandido');
+  }
+  else {
+    e.currentTarget.innerText = '+';
+    e.currentTarget.parentElement.classList.remove('expandido');
+  }
+}
+
+for (let botao of botoes) {
+  botao.addEventListener('click', botaoClicado);
+}
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,25 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+let imagem = document.querySelector('#slide');
+let botaoAnterior = document.querySelector('#anterior');
+let botaoProximo = document.querySelector('#proximo');
+
+botaoAnterior.addEventListener('click', () => {
+  let posicao = todasAsImagens.indexOf(imagem.getAttribute('src').split('/').pop()) - 1;
+  if (posicao < 0) {
+    posicao = todasAsImagens.length - 1;
+  }
+  let enderecoDaImagem = servidorDasImagens + todasAsImagens[posicao];
+  imagem.setAttribute('src', enderecoDaImagem);
+});
+
+botaoProximo.addEventListener('click', () => {
+  let posicao = todasAsImagens.indexOf(imagem.getAttribute('src').split('/').pop()) + 1;
+  if (posicao >= todasAsImagens.length) {
+    posicao = 0;
+  }
+  let enderecoDaImagem = servidorDasImagens + todasAsImagens[posicao];
+  imagem.setAttribute('src', enderecoDaImagem);
+});
